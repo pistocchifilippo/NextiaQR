ba052dc70c27363946ad187407f82f2aec3b0959
pistocchifilippo <filippo.pistocchi4@studio.unibo.it>
2022-02-05 12:17:58 +0100

    ToSQL

--- src/test/java/edu/upc/essi/dtim/utils/RunRollUpQueries.java
+++ src/test/java/edu/upc/essi/dtim/utils/RunRollUpQueries.java
@@ -14,6 +14,7 @@ import org.apache.jena.tdb.TDBFactory;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -44,25 +45,41 @@ public class RunRollUpQueries {
         T.begin(ReadWrite.READ);
 
         final Tuple2<String, String> minimal = queries.remove(0);
+        final List<Set<ConjunctiveQuery>> CQs = new LinkedList<>();
+        final List<String> SQLs = new LinkedList<>();
         for (Tuple2<String, String> query : queries) {
             System.out.println(query._2());
             System.out.println(minimal._2());
             //1 -- Rewrite SPARQL to UCQs
-            Set<ConjunctiveQuery> CQs = NextiaQR.rewriteToUnionOfConjunctiveQueries(query._2,T,minimal._2);
-            System.out.println(CQs);
+            Set<ConjunctiveQuery> CQ = NextiaQR.rewriteToUnionOfConjunctiveQueries(query._2,T,minimal._2);
+            System.out.println(CQ);
             System.out.println();
-            T.end();
-            T.close();
-            FileUtils.deleteDirectory(new File("TestScenarioRunnerDataset"));
-            prefixes = TestUtils.populatePrefixes(scenarioPath + "prefixes.txt");
-            TestUtils.populateTriples(jenaPath, baseURI, scenarioPath + "metamodel.txt", prefixes);
-            TestUtils.populateTriples(jenaPath, baseURI, scenarioPath + "global_graph.txt", prefixes);
-            TestUtils.populateTriples(jenaPath, baseURI,scenarioPath+"source_graph.txt", prefixes);
-            TestUtils.populateMappings(jenaPath,scenarioPath+"mappings.txt",scenarioPath + "global_graph.txt", prefixes);
-            T = TDBFactory.createDataset("TestScenarioRunnerDataset");
-            T.begin(ReadWrite.READ);
 
+            /*
+            Map<String,String> iriToCSVPath = Maps.newHashMap();
+            Files.readAllLines(new File(scenarioPath+"wrappers_files.txt").toPath()).stream().forEach(s->{
+                iriToCSVPath.put(s.split(",")[0],s.split(",")[1]);
+            });
+            CQ.forEach(cq -> {
+                Set<Wrapper> CSV_Wrappers = Sets.newHashSet();
+                cq.getWrappers().forEach(w -> {
+                    CSV_Wrapper csv = new CSV_Wrapper(w.getWrapper());
+                    csv.setPath(iriToCSVPath.get(w.getWrapper()));
+                    csv.setHeaderInFirstRow(true);
+                    csv.setColumnDelimiter(",");
+                    csv.setRowDelimiter("\n");
+                    CSV_Wrappers.add(csv);
+                });
+                cq.setWrappers(CSV_Wrappers);
+            });
+            String SQL = NextiaQR.toSQL(CQ,null);
+            System.out.println(SQL);
+            CQs.add(CQ);
+             */
         }
+
+
+
         T.end();
         T.close();
 
